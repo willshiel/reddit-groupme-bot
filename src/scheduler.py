1c879d69cb9bdc6bdd6d6fe204e7a032d13d92f8
willshiel <ws248531@muhlenberg.edu>
2018-07-05 18:53:36 -0400

    add logging

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,8 +1,31 @@
 from flask import Flask
 import time
 import atexit
+import logging
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 app = Flask(__name__)
+log = logging.getLogger(__name__)
+
+def make_request():
+    '''
+        Makes the official request to the reddit api
+        in order to get the 15 most popular posts
+    '''
+    log.debug("This is not yet implemented")
+    print("This is not implemented yet.")
+    return NotImplemented
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=make_request,
+    trigger=IntervalTrigger(minutes=5),
+    id='reddit_request',
+    name='Make request to /r/soccer every five minutes',
+    replace_existing=True
+)
+
+atexit.register(lambda: scheduler.shutdown())
