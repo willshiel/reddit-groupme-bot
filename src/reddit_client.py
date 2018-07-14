25c1725757f0540927e999342f450d252892fbe7
willshiel <ws248531@muhlenberg.edu>
2018-07-14 19:03:06 -0400

    further progress

--- src/reddit_client.py
+++ src/reddit_client.py
@@ -4,7 +4,29 @@ import praw
 import logging
 
 
-reddit = praw.Reddit(client_id=secrets.CLIENT_ID, client_secret=secrets.CLIENT_SECRET, password=secrets.PASSWORD, user_agent=secrets.USER_AGENT, username=secrets.USERNAME)
+def get_reddit():
+    try:
+        reddit = praw.Reddit(
+            client_id=secrets.CLIENT_ID, 
+            client_secret=secrets.CLIENT_SECRET, 
+            password=secrets.PASSWORD, 
+            user_agent=secrets.USER_AGENT, 
+            username=secrets.USERNAME
+        )
 
-for submission in reddit.subreddit('soccer').hot(limit=10):
-    print(submission.title)
+        return reddit
+    except:
+        logging.error("Unable to connect to reddit")
+
+
+def make_request(reddit):
+    logging.warning("made it here!")
+    try:
+        if reddit is None:
+            reddit = get_reddit()
+        for submission in reddit.subreddit('soccer').hot(limit=25):
+            if submission != None:
+                if submission.ups > 10000:
+                    print(submission.title)
+    except:
+        logging.error("Unable to get submissions")
--- src/scheduler.py
+++ src/scheduler.py
@@ -1,24 +1,13 @@
-from flask import Flask
 import time
-import atexit
 import logging
+from reddit_client import make_request, get_reddit
+import threading
 
-from reddit_client import make_request, get_auth_token
+reddit = get_reddit()
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
+def call_reddit():
+    t = threading.Timer(5.0, make_request, args=(reddit,))
+    t.daemon = True
+    t.start()
 
-app = Flask(__name__)
-
-scheduler = BackgroundScheduler()
-scheduler.start()
-
-scheduler.add_job(
-    func=make_request,
-    trigger=IntervalTrigger(seconds=30),
-    id='reddit_request',
-    name='Make request to /r/soccer every five minutes',
-    replace_existing=True
-)
-
-atexit.register(lambda: scheduler.shutdown())
+call_reddit()
