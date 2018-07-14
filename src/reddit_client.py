95e0958bce1cf9f9b8aa612d06fca34f9e6ebda6
willshiel <ws248531@muhlenberg.edu>
2018-07-14 16:53:48 -0400

    able to get top ten posts

--- src/reddit_client.py
+++ src/reddit_client.py
@@ -1,30 +1,10 @@
-from secrets import PASSWORD
+import time
+import secrets
+import praw
 import logging
-import requests
-import requests.auth
-
-logging.basicConfig(level=logging.WARN)
-
-REDDIT_URL = "http://www.reddit.com/r/redditdev/new.json?sort=new"
-USER_AGENT = "RedditGroupMeClient/0.1 by RaulBravo"
-
-def get_auth_token():
-    client_auth = requests.auth.HTTPBasicAuth('8W0rxUUWJV6rHg', 'GKiyEej3EaC3OXtKsyXisI4vt70')
-    post_data = {"grant_type": "password", "username": "raulbravo", "password": PASSWORD}
-    headers = {"User-Agent": USER_AGENT}
-    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-    return response
 
 
-def make_request():
-    '''
-        Makes the official request to the reddit api
-        in order to get the 15 most popular posts
-    '''
-    auth_token = get_auth_token().json()
-    headers = {'Authorization': 'bearer {}'.format(auth_token['access_token']), 'User-Agent': USER_AGENT}
-    logging.warn(headers)
-    response = requests.get(REDDIT_URL, headers=headers).json()
-    logging.warn("This is the response: {}".format(response))
+reddit = praw.Reddit(client_id=secrets.CLIENT_ID, client_secret=secrets.CLIENT_SECRET, password=secrets.PASSWORD, user_agent=secrets.USER_AGENT, username=secrets.USERNAME)
 
-make_request()
+for submission in reddit.subreddit('soccer').hot(limit=10):
+    print(submission.title)
