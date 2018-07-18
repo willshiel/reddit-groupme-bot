9b0d695bec6cea5e2d1a0dfe9236f57fd5a97517
willshiel <ws248531@muhlenberg.edu>
2018-07-17 21:40:06 -0400

    add cache for posts

--- src/reddit_client.py
+++ src/reddit_client.py
@@ -6,7 +6,7 @@ import logging
 
 def get_reddit():
     try:
-        reddit = praw.Reddit(
+        return praw.Reddit(
             client_id=secrets.CLIENT_ID, 
             client_secret=secrets.CLIENT_SECRET, 
             password=secrets.PASSWORD, 
@@ -14,18 +14,22 @@ def get_reddit():
             username=secrets.USERNAME
         )
 
-        return reddit
     except:
         logging.error("Unable to connect to reddit")
 
 
 def make_request(reddit):
+    hot_submissions = []
     try:
         for submission in reddit.subreddit('soccer').hot(limit=25):
             if submission != None:
                 if submission.ups > 5000:
-                    print(submission.title)
+                    hot_submissions.append(submission)
             else:
                 logging.debug("There were no posts that were over the threshold.")
+            
+        
+        return hot_submissions
+
     except:
         logging.error("Unable to get submissions")
--- src/scheduler.py
+++ src/scheduler.py
@@ -4,8 +4,22 @@ from reddit_client import make_request, get_reddit
 
 reddit = get_reddit()
 
-while True:
-    logging.debug("Making request.")
-    make_request(reddit)
-    logging.debug("Waiting five seconds to make next request.")
-    time.sleep(5 * 60)
+cached_posts = []
+
+def add_submissions_to_cache(submissions):
+    for submission in submissions:
+        if submission not in cached_posts:
+            cached_posts.append(submission)
+            
+        # make call to database here
+
+
+def main():
+    while True:
+        logging.debug("Making request.")
+        submissions = make_request(reddit)
+        add_submissions_to_cache(submissions)
+        logging.debug("Waiting five seconds to make next request.")
+        time.sleep(5 * 60)
+
+main()
