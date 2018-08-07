2fada95a0134dae95d238e4227dcd7e53c661623
willshiel <ws248531@muhlenberg.edu>
2018-08-06 20:45:32 -0400

    style changes

--- src/reddit_client.py
+++ src/reddit_client.py
@@ -1,39 +1,39 @@
-import time
 import secrets
 import praw
 import logging
 from utils import convert_to_submission
 from config import get_base_logging_directory
 
-logging.basicConfig(filename=get_base_logging_directory() + 'reddit_client.log', level=logging.ERROR)
+logging.basicConfig(filename=get_base_logging_directory() + 'reddit_client.log',
+                    level=logging.ERROR)
+
 
 class RedditClient(object):
 
     def __init__(self):
         try:
             self.reddit = praw.Reddit(
-                client_id=secrets.CLIENT_ID, 
-                client_secret=secrets.CLIENT_SECRET, 
-                password=secrets.PASSWORD, 
-                user_agent=secrets.USER_AGENT, 
+                client_id=secrets.CLIENT_ID,
+                client_secret=secrets.CLIENT_SECRET,
+                password=secrets.PASSWORD,
+                user_agent=secrets.USER_AGENT,
                 username=secrets.USERNAME
             )
 
         except:
             logging.error("Unable to connect to reddit", exc_info=True)
 
-
     def get_hot_submissions(self):
         hot_submissions = []
         try:
             for submission in self.reddit.subreddit('soccer').hot(limit=25):
-                if submission != None:
+                if submission is not None:
                     if submission.ups > 5000:
                         hot_submissions.append(convert_to_submission(submission))
                 else:
-                    logging.debug("There were no posts that were over the threshold.", exc_info=True)
-                
-            
+                    logging.debug("There were no posts that were over the threshold.",
+                                  exc_info=True)
+
             return hot_submissions
 
         except:
