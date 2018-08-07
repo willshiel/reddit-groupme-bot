ef9ae6efb9c8b2fda2f450f23cc0dcbd657ed231
willshiel <ws248531@muhlenberg.edu>
2018-08-06 20:45:05 -0400

    include dates in models

--- src/app.py
+++ src/app.py
@@ -4,11 +4,14 @@ from database_client import DatabaseClient
 from groupme_client import GroupMeClient
 from collections import deque
 from config import get_base_logging_directory
+import time
 import logging
 
-logging.basicConfig(filename=get_base_logging_directory() + 'app.log', level=logging.ERROR)
+logging.basicConfig(filename=get_base_logging_directory() + 'app.log',
+                    level=logging.ERROR)
 
 queue = deque(maxlen=100)
+
 
 def start():
     reddit = None
@@ -24,8 +27,9 @@ def start():
             db.insert_submission(sub)
             print("posting messages")
             groupme_client.post_submission(sub)
-        
+
         time.sleep(5 * 60)
+
 
 def _remove_duplicates(submissions):
     new_subs = []
@@ -33,8 +37,9 @@ def _remove_duplicates(submissions):
         if submission is not None and submission['id'] not in queue:
             queue.append(submission['id'])
             new_subs.append(submission)
-    
+
     return new_subs
+
 
 def _instantiate_clients(reddit, db):
     try:
--- src/models.py
+++ src/models.py
@@ -1,9 +1,15 @@
+from datetime import datetime
+
+
 class Submission(dict):
     """Reddit submission object"""
-    def __init__(self, id, title, ups, downs, author, url):
+    def __init__(self, id, title, ups, downs, author, url,
+                 last_modified=datetime.now(), created=datetime.now()):
         self.__setitem__('id', id)
         self.__setitem__('title', title)
         self.__setitem__('ups', ups)
         self.__setitem__('downs', downs)
         self.__setitem__('author', author)
         self.__setitem__('url', url)
+        self.__setitem__('lastModified', last_modified)
+        self.__setitem__('created', created)
